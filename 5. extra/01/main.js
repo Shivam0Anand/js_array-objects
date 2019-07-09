9964b5cf89a161dd25074a4c3548bcef5275cee4
Shivam0Anand <shivam_anand@outlook.com>
2019-07-09 16:55:06 +0530

    update

--- 5. extra/01/main.js
+++ 5. extra/01/main.js
@@ -1,1 +1,50 @@
 // Your solution
+// 1
+
+function animalTestUser(username) {
+  var obj = {
+    username
+  };
+  return obj;
+}
+
+// 2
+
+function animalTestUser(username) {
+  var obj = {
+    username
+  };
+
+  obj.otherArgs = [];
+  if (arguments.length < 1) {
+    for (var i = 1; i < arguments.length; i++) {
+      obj.otherArgs.push(arguments[i]);
+    }
+  }
+  console.log(obj);
+}
+
+//3
+function AnimalCreator(username, species, tagline, noises) {
+  return {
+    username: username,
+    species: species,
+    tagline: tagline,
+    noises: noises
+  };
+}
+
+// 4
+
+var cow = AnimalCreator("Moo", "Cow", "You cant count me!", ["maahhh"]);
+function addFriend(sheep, cow) {
+  sheep.friend.push(cow);
+}
+
+// 5
+
+function addFriend(sheep, cow) {
+  sheep.friend.push(cow.username);
+}
+
+//6
